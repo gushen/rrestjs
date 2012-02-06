d0fae1904775827e838740e490ce1e20d3968ee7
spout <snoopyxdy@gmail.com>
2012-02-06 09:39:36 +0800

    multi upload

--- app/controller/dropfile.js
+++ app/controller/dropfile.js
@@ -4,6 +4,10 @@ home.index = function(req, res){
 	res.sendfile(__dirname+'/../static/skin/dropExample/droptest.html');
 	return;
 }
+home.multi = function(req, res){
+	res.sendfile(__dirname+'/../static/skin/dropExample/multiUpload.html');
+	return;
+}
 home.upload = function(req, res){
 	var size = Math.ceil((req.headers['content-length']-0)/1024)+'kb';
 	res.sendjson({res: 'success', size:size});
